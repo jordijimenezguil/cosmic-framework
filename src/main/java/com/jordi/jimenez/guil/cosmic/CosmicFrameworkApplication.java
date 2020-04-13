87bc5e54a63b2eaf246648e167e166705bbe74a6
jordi-jimenez <jordi.jimenez@scmspain.com>
2020-04-13 12:52:49 +0200

    Initial structure code

---
+++ src/main/java/com/jordi/jimenez/guil/cosmic/CosmicFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.jordi.jimenez.guil.cosmic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmicFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordi/jimenez/guil/cosmic/CosmicFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordi.jimenez.guil.cosmic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmicFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
